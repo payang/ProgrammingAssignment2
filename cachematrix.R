6d3f505a5f45ff5d0be4700c18599321fe8a0c75
Patrick Yang <ypat90@hotmail.com>
2014-08-09 12:00:44 -0400

    done

--- cachematrix.R
+++ cachematrix.R
@@ -3,21 +3,24 @@
 ## a matrix rather than computing it repeatedly.
 ## The following pair of functions caches the inverse of a matrix.
 
+## Assumption: *** The matrix supplied is always invertible ***
+
+
 # This function creates a special "matrix" object
 # that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) { #embedded functions
     m <- NULL
-    set <- function(y) {
+    set <- function(y) {        #assigns source matrix
         x <<- y
         m <<- NULL
     }
-    get <- function() x
-    setmatrix <- function(solve) m <<- solve
-    getmatrix <- function() m
+    get <- function() x         #read source matrix
+    setmatrix <- function(solve) m <<- solve #store inverse in cache
+    getmatrix <- function() m   #peek inside cache
     list(set = set, get = get,
          setmatrix = setmatrix,
-         getmatrix = getmatrix)
+         getmatrix = getmatrix) #create list
 }
 
 
@@ -27,13 +30,29 @@ makeCacheMatrix <- function(x = matrix()) {
 #the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x = matrix(), ...) {
-    m <- x$getmatrix()
-    if(!is.null(m)) {
-        message("getting cached data")
-        return(m)
+    m <- x$getmatrix()      #attempt to read cache
+    if(!is.null(m)) {       #check content of cache, if not null then proceeds
+        message("getting cached data") #output confirmation cache is used
+        return(m)           #exit function and outputs inverse matrix
     }
-    matrix <- x$get()
-    m <- solve(matrix, ...)
-    x$setmatrix(m)
-    m
+    matrix <- x$get()       #load source matrix
+    m <- solve(matrix, ...) #calculate inverse matrix
+    x$setmatrix(m)          #call function to set cache
+    m                       #output inverse matrix from cache or new calculation
 }
+
+
+
+# # To test functions uncomment and run code below
+# cat("\014")                 #clear console in RStudio
+# m1 <- matrix(rnorm(16,2,10),4,4)         #assign matrix to m1
+# m1                          #display source matrix
+# m2 <- makeCacheMatrix(m1)   #create cached matrix 
+# m2$get()                    #confirms source matrix used
+# m2$getmatrix()              #peek inside cache
+# m2$set(m1)                  #sets value of matrix to be inverted
+# m2$getmatrix()              #peek inside cache
+# cacheSolve(m2)              #resolve inverse and display
+# cacheSolve(m2)              #resolve inverse with confirmation cache was used 
+# m2$getmatrix()              #peek inside cache, now shows value
+# round(m1 %*% m2$getmatrix())       #check multiplication returns identity matrix
